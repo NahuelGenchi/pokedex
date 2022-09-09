720007829ab3babb5d1882e4e63a664d15cb9212
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-09-08 22:34:25 -0300

    adding color to pokemon type name

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -35,7 +35,33 @@ const Item = (props) => {
       <div className="item-c-types">
         {pokemon.types.map((pokeType, index) => {
           return(
-            <span key={index}>{pokeType.type.name}</span>
+            <span
+              key={index}
+              style={{
+                backgroundColor: (
+                  (pokeType.type.name === "grass" && "#8BD169") ||
+                  (pokeType.type.name === "poison" && "#B76DA4") ||
+                  (pokeType.type.name === "fire" && "#FF603F") ||
+                  (pokeType.type.name === "flying" && "#8995DB") ||
+                  (pokeType.type.name === "water" && "#52A8FA") ||
+                  (pokeType.type.name === "bug" && "#B7C53F") ||
+                  (pokeType.type.name === "normal" && "#B7B6A4") ||
+                  (pokeType.type.name === "dark" && "#8C6E5C") ||
+                  (pokeType.type.name === "electric" && "#FFD34E") ||
+                  (pokeType.type.name === "ground" && "#E2C56A") ||
+                  (pokeType.type.name === "psychic" && "#FF6DA4") ||
+                  (pokeType.type.name === "ice" && "#7DD3FA") ||
+                  (pokeType.type.name === "steel" && "#A9A8B1") ||
+                  (pokeType.type.name === "fighting" && "#C13023") ||
+                  (pokeType.type.name === "fairy" && "#EE9BEE") ||
+                  (pokeType.type.name === "rock" && "#B89F38") ||
+                  (pokeType.type.name === "dragon" && "#713BFA") ||
+                  (pokeType.type.name === "ghost" && "#715996")
+                )
+              }}
+            >
+              {pokeType.type.name}
+            </span>
           );
         })}
       </div>
