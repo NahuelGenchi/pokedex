a3f0aac8b8f2297b69531109123e2b96113842d5
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-09-09 23:13:26 -0300

    adding reactrouter to Item components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ItemList } from "./components";
 
 import "./scss/main.scss";
 
 const App = () => {
   return (
-    <>
-      <ItemList/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ItemList/>}/>
+        <Route path="/pokemon" element={<ItemList/>}/>
+        <Route path="/pokemon/:name" element={"Pokemon"}/>
+        <Route path="*" element={"Invalid path. Please enter a valid URL."}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import "./Item.scss";
 
@@ -21,51 +22,53 @@ const Item = (props) => {
     return "Loading Pokemon...";
   };
 
-  return(
-    <div className="item-container">
-      <div className="item-c-img">
-        <img src={pokemon.sprites.front_default} alt="Pokémon sprite"/>
+  return (
+    <Link to={`/pokemon/${pokemon.name}`}>
+      <div className="item-container">
+        <div className="item-c-img">
+          <img src={pokemon.sprites.front_default} alt="Pokémon sprite" />
+        </div>
+        <div className="item-c-id">
+          #{pokemon.id}
+        </div>
+        <div className="item-c-name">
+          {pokemon.name}
+        </div>
+        <div className="item-c-types">
+          {pokemon.types.map((pokeType, index) => {
+            return (
+              <span
+                key={index}
+                style={{
+                  backgroundColor: (
+                    (pokeType.type.name === "grass" && "#8BD169") ||
+                    (pokeType.type.name === "poison" && "#B76DA4") ||
+                    (pokeType.type.name === "fire" && "#FF603F") ||
+                    (pokeType.type.name === "flying" && "#8995DB") ||
+                    (pokeType.type.name === "water" && "#52A8FA") ||
+                    (pokeType.type.name === "bug" && "#B7C53F") ||
+                    (pokeType.type.name === "normal" && "#B7B6A4") ||
+                    (pokeType.type.name === "dark" && "#8C6E5C") ||
+                    (pokeType.type.name === "electric" && "#FFD34E") ||
+                    (pokeType.type.name === "ground" && "#E2C56A") ||
+                    (pokeType.type.name === "psychic" && "#FF6DA4") ||
+                    (pokeType.type.name === "ice" && "#7DD3FA") ||
+                    (pokeType.type.name === "steel" && "#A9A8B1") ||
+                    (pokeType.type.name === "fighting" && "#C13023") ||
+                    (pokeType.type.name === "fairy" && "#EE9BEE") ||
+                    (pokeType.type.name === "rock" && "#B89F38") ||
+                    (pokeType.type.name === "dragon" && "#713BFA") ||
+                    (pokeType.type.name === "ghost" && "#715996")
+                  )
+                }}
+              >
+                {pokeType.type.name}
+              </span>
+            );
+          })}
+        </div>
       </div>
-      <div className="item-c-id">
-        #{pokemon.id}
-      </div>
-      <div className="item-c-name">
-        {pokemon.name}
-      </div>
-      <div className="item-c-types">
-        {pokemon.types.map((pokeType, index) => {
-          return(
-            <span
-              key={index}
-              style={{
-                backgroundColor: (
-                  (pokeType.type.name === "grass" && "#8BD169") ||
-                  (pokeType.type.name === "poison" && "#B76DA4") ||
-                  (pokeType.type.name === "fire" && "#FF603F") ||
-                  (pokeType.type.name === "flying" && "#8995DB") ||
-                  (pokeType.type.name === "water" && "#52A8FA") ||
-                  (pokeType.type.name === "bug" && "#B7C53F") ||
-                  (pokeType.type.name === "normal" && "#B7B6A4") ||
-                  (pokeType.type.name === "dark" && "#8C6E5C") ||
-                  (pokeType.type.name === "electric" && "#FFD34E") ||
-                  (pokeType.type.name === "ground" && "#E2C56A") ||
-                  (pokeType.type.name === "psychic" && "#FF6DA4") ||
-                  (pokeType.type.name === "ice" && "#7DD3FA") ||
-                  (pokeType.type.name === "steel" && "#A9A8B1") ||
-                  (pokeType.type.name === "fighting" && "#C13023") ||
-                  (pokeType.type.name === "fairy" && "#EE9BEE") ||
-                  (pokeType.type.name === "rock" && "#B89F38") ||
-                  (pokeType.type.name === "dragon" && "#713BFA") ||
-                  (pokeType.type.name === "ghost" && "#715996")
-                )
-              }}
-            >
-              {pokeType.type.name}
-            </span>
-          );
-        })}
-      </div>
-    </div>
+    </Link>
   );
 };
 
